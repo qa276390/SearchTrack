b89e109bb2ce5b47287c50a30041f3a5c9e26108
qa276390 <qa276390@gmail.com>
2021-11-18 12:52:02 +0800

    set default age to 1

--- src/lib/opts.py
+++ src/lib/opts.py
@@ -254,7 +254,7 @@ class opts(object):
     self.parser.add_argument('--no_pre_img', action='store_true')
     self.parser.add_argument('--zero_tracking', action='store_true')
     self.parser.add_argument('--hungarian', action='store_true')
-    self.parser.add_argument('--max_age', type=str, default="2") # tracker alive thershold when inference
+    self.parser.add_argument('--max_age', type=str, default="1") # tracker alive thershold when inference
     self.parser.add_argument('--num_pre_data', type=int, default=1) # when training 
     self.parser.add_argument('--att_hm_disturb', type=float, default=0)
     self.parser.add_argument('--att_lost_disturb', type=float, default=0)
